3e4ded072092160c3d3225c8c83a7a571234a853
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-03-16 09:32:00 -0600

    fix spacing and add element to prime method

--- fibonnaci.rb
+++ fibonnaci.rb
@@ -10,28 +10,28 @@
 
 # prime method
 
-  def is_prime?(num)
-    if num < 0 
-      false
-    elsif num <= 3 || !((2..Math.sqrt(num).floor).to_a.any?{|x| num % x == 0})
-      true
-    else
-      false
-    end
+def is_prime?(num)
+  if num < 0 || num.even?
+    false
+  elsif num <= 3 || !((2..Math.sqrt(num).floor).to_a.any?{|x| num % x == 0})
+    true
+  else
+    false
   end
+end
 
 #p fibonnaci method
-  (1..25).each do |num| 
-    p fibonnaci(num)
-  end
+(1..25).each do |num| 
+  p fibonnaci(num)
+end
 
 #create array for prime fibonnacis
-  prime_fib_array = (1..25).to_a.map do |num| 
-    fibonnaci(num)
-  end
+prime_fib_array = (1..25).to_a.map do |num| 
+  fibonnaci(num)
+end
 
 #p prime fibonnaci
-  prime_fib_array.each do |num|
-    p num if is_prime?(num)
-  end
-  
+prime_fib_array.each do |num|
+  p num if is_prime?(num)
+end
+
